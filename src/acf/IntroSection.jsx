54e660e4793dcb593ebcc3c3d42b36750bb59c84
samlogan <sam@sjlwebdesign.com>
2019-01-16 10:01:30 +1100

    Removed component level static queries

--- src/acf/IntroSection.jsx
+++ src/acf/IntroSection.jsx
@@ -1,38 +1,13 @@
 import React from 'react';
-import { StaticQuery, graphql } from 'gatsby';
-import { getComponentData } from './utils';
 
 export const IntroSection = (props) => {
-  const { id } = props;
+  const { title, subtitle } = props;
   return (
-    <StaticQuery
-      query={graphql`
-        query {
-          allIntroSections: allWordPressAcfIntroSection {
-            edges {
-              node {
-                id,
-                title,
-                subtitle,
-              }
-            }
-          },
-        }
-      `}
-      render={data => {
-        const { allIntroSections } = data;
-        const introSection = getComponentData(allIntroSections, id);
-        if (!introSection) return null;
-        const { title, subtitle } = introSection;
-        return (
-          <section className="intro-section">
-            <div className="wrap">
-              <h1>{title}</h1>
-              <h2>{subtitle}</h2>
-            </div>
-          </section>
-        )
-      }}
-    />
-  );
+    <section className="intro-section">
+      <div className="wrap">
+        <h1>{title}</h1>
+        <h2>{subtitle}</h2>
+      </div>
+    </section>
+  )
 }
